da4b5e12ee636eb0667690a1313756913ef64794
krishnaj01 <krishna.jhanwar2005@gmail.com>
2025-01-07 13:51:19 +0530

    cookie config changes

--- server/utils/generateJWTandSetCookie.js
+++ server/utils/generateJWTandSetCookie.js
@@ -11,7 +11,7 @@ export const generateJWTandSetCookie = (res, userId) => {
 
     res.cookie('jwt-token', token, {
         httpOnly: true, // prevents XSS attacks
-        sameSite: 'strict', // prevents CSRF attacks
+        sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'strict', // prevents CSRF attacks (as my backend and frontend are on different deployment therefore lax is preferred)
         secure: process.env.NODE_ENV === 'production', //for HTTPS
         maxAge: 1000 * 60 * 60 * 24 * 7,
         path: '/',
